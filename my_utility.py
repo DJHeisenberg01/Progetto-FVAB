55ded4a61ddab0b01082249f65f558439911f0a8
Dar7oo <143949654+Dar7oo@users.noreply.github.com>
2024-05-11 13:09:42 +0200

    Added scipy distances
    To do:
    - test network on single test set observation
    - fix mahalanobis and seuclidean distances

--- my_utility.py
+++ my_utility.py
@@ -1,17 +1,17 @@
 import numpy as np
-from keras import ops # DA TOGLIERE
-#import matplotlib.pyplot as plt
-import tensorflow as tf # DA TOGLIERE
 import os
 import cv2
 import numpy as np
-
-#from scipy.spatial.distance import cityblock
-
 import pandas as pd
+
 import torch
 import torch.nn.functional as F
 from torch.utils.data import Dataset
+
+from scipy.spatial.distance import euclidean, cityblock, seuclidean, canberra, mahalanobis, chebyshev, braycurtis
+
+def rgb2gray(rgb):
+    return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
 
 
 class ImageDataSet(Dataset):
@@ -92,138 +92,92 @@ class CustomLoss(torch.nn.Module):
         self.distance = distance
         
     def forward(self, output, label):
-        # Calcola la distanza tra embedding previsto dalla rete ed embedding reale
-        loss = self.distance(output, label)
+        batch_distances = np.zeros(shape=(16,), dtype=np.float32)
+        
+        # Calcola la distanza tra embedding previsto dalla rete ed embedding reale per tutte gli elementi della batch
+        for i in range(output.shape[0]):
+            temp = self.distance(output[i], label[i])
+            batch_distances[i] = temp
+
         #print(f"loss: {loss}")
         #print(f"loss.shape: {loss.shape}")
-        loss = loss.mean()
-        #print(f"loss.shape: {loss.shape}")
+        #print(f"type(loss): {type(loss)}")
         
+        loss = torch.tensor(batch_distances, dtype=torch.float32, device=output.device)
+        loss = loss.mean()
+        #print(f"loss: {loss}")
+        #print(f"loss.shape: {loss.shape}")        
         return loss
 
-#def open_csv(dir,file_name,n):
-#    with open(dir+file_name) as csv_file:
-#        dataread = list(csv.reader(csv_file, delimiter=','))
-#        fulldata = [x for x in dataread if x != []]
-#        arr_created = np.asarray(fulldata, dtype=np.float32)
-#        tensor_created = torch.from_numpy(arr_created)
-#        shape = (n, 384)  # the array size depend on the dimension of the codec
-#        data_ar = torch.reshape(tensor_created, shape)
-#        return data_ar
-#
-#
-#def image_loader(dir_images):#,dir_codify):
-#    list_images=[]
-#    count=0
-#    for file_name in os.listdir(dir_images):
-#        if not file_name.startswith('.'):
-#            count=count+1
-#            codify_name=file_name + '.csv'
-#            img = cv2.imread(os.path.join(dir_images, file_name))
-#            img = cv2.resize(img, (128, 128))
-#            img = rgb2gray(img)
-#            img_array=np.asarray(img, dtype=np.float32)
-#            list_images.append(img_array)
-#
-#    tot_images = np.asarray(list_images)
-#    tot_images = torch.from_numpy(tot_images)
-#    print(tot_images.shape)
-#    print(type(tot_images))   
-#    return tot_images
 
-
-def rgb2gray(rgb):
-    return np.dot(rgb[...,:3], [0.299, 0.587, 0.144])
-
-def euclidean_distance(vects):
-    x, y = vects
-    return tf.sqrt(tf.reduce_sum(tf.square(x - y), axis=1, keepdims=True))
-
-def manhattan_distance(vects):
-    x, y = vects   
-    return tf.reduce_sum(tf.abs(x - y), axis=1, keepdims=True)
-
-def canberra_distance(vects):
-    x, y = vects
-    numerator = tf.abs(x - y)
-    denominator = tf.abs(x) + tf.abs(y)
-    return tf.reduce_sum(numerator / denominator, axis=1, keepdims=True)
-
-def hamming_distance(vects):
-    x, y = vects
+def custom_euclidean_distance(u, v):
+    # Trasformazione degli oggetti in ndarray per poter usare la funzione di scipy.spatial.distance    
+    u = u.cpu().detach().numpy().flatten()
+    v = v.cpu().detach().numpy().flatten()
     
+    return euclidean(u, v)
+
+
+def custom_manhattan_distance(u, v):
+    # Trasformazione degli oggetti in ndarray per poter usare la funzione di scipy.spatial.distance    
+    u = u.cpu().detach().numpy().flatten()
+    v = v.cpu().detach().numpy().flatten()
     
-    #return tf.reduce_mean(tf.cast(tf.not_equal(x1, y1), tf.float32), axis=-1, keepdims=True)
-    distance = np.mean(np.not_equal(x, y))
+    return cityblock(u, v)
+
+
+
+def custom_seuclidean_distance(u, v):
+    # Calcolo della matrice di varianza e covarianza tra i due input
+    mat = torch.cat((u, v))
+    cov_mat = torch.cov(mat)
     
-    return distance
+    # Prendo la diagonale
+    diag = torch.diagonal(cov_mat)
 
-def chebyshev_distance(vects):
-    x, y = vects
-    chebyshev_diff = tf.abs(x - y)
-    chebyshev_max = tf.reduce_max(chebyshev_diff, axis=1)
-    return chebyshev_max
-
-def bray_curtis_distance(vects):
-    x, y = vects
-    sum_diff = tf.reduce_sum(tf.abs(x - y), axis=1, keepdims=True)
-    sum_sum = tf.reduce_sum(tf.abs(x + y), axis=1, keepdims=True)
+    # Trasformazione degli oggetti in ndarray per poter usare la funzione di scipy.spatial.distance    
+    u = u.cpu().detach().numpy().flatten()
+    v = v.cpu().detach().numpy().flatten()
     
-    return tf.divide(sum_diff, sum_sum)
+    diag = diag.cpu().detach().numpy().flatten()
+    
+    return seuclidean(u, v, diag)
+    
+
+def custom_canberra_distance(u, v):
+    # Trasformazione degli oggetti in ndarray per poter usare la funzione di scipy.spatial.distance    
+    u = u.cpu().detach().numpy().flatten()
+    v = v.cpu().detach().numpy().flatten()
+    
+    return canberra(u, v)
 
 
-#covariance_matrix da definire
-def mahalanobis_distance(vects, covariance_matrix):
-    x, y = vects
-    diff = x - y
-    diff_transpose = tf.transpose(diff)
-    mahalanobis = tf.linalg.matmul(diff, tf.linalg.inv(covariance_matrix))
-    mahalanobis = tf.linalg.matmul(mahalanobis, diff_transpose)
-    return tf.sqrt(mahalanobis)
-
-def cosine_distance(vects):
-    x, y = vects
-    cos = tf.keras.losses.CosineSimilarity(axis=-1, reduction="sum_over_batch_size", name="cosine_similarity")
-    return cos.call(x,y)
-
-def standard_euclidean(vects):
-    x, y = vects
-    #manca il vettore delle varianze
-    return tf.sqrt(tf.reduce_sum(tf.square(x - y), axis=1, keepdims=True))
+def custom_mahalanobis_distance(u, v):
+    # Calcolo della matrice V^-1, l'inversa della matrice di varianza e covarianza tra i due input
+    mat = torch.cat((u, v))
+    cov_mat = torch.cov(mat)
+    cov_mat_inv = torch.inverse(cov_mat)
+    
+    # Trasformazione degli oggetti in ndarray per poter usare la funzione di scipy.spatial.distance
+    u = u.cpu().detach().numpy().flatten()
+    v = v.cpu().detach().numpy().flatten()
+    
+    cov_mat_inv = cov_mat_inv.cpu().detach().numpy()
+    
+    return mahalanobis(u, v, cov_mat_inv)
 
 
-
-def similarity_accuracy(y_true, y_pred, threshold=0.5):
-    return tf.keras.metrics.binary_accuracy(tf.cast(y_true < threshold, tf.float32), tf.cast(y_pred < threshold, tf.float32))
-
-def loss(margin=1):
-    """Provides 'contrastive_loss' an enclosing scope with variable 'margin'.
-
-    Arguments:
-        margin: Integer, defines the baseline for distance for which pairs
-                should be classified as dissimilar. - (default is 1).
-
-    Returns:
-        'contrastive_loss' function with data ('margin') attached.
-    """
-
-    # Contrastive loss = mean( (1-true_value) * square(prediction) +
-    #                         true_value * square( max(margin-prediction, 0) ))
-    def contrastive_loss(y_true, y_pred):
-        """Calculates the contrastive loss.
-
-        Arguments:
-            y_true: List of labels, each label is of type float32.
-            y_pred: List of predictions of same length as of y_true,
-                    each label is of type float32.
-
-        Returns:
-            A tensor containing contrastive loss as floating point value.
-        """
-        square_pred = ops.square(y_pred)
-        margin_square = ops.square(ops.maximum(margin - (y_pred), 0))
-        return ops.mean((1 - y_true) * square_pred + (y_true) * margin_square)
-
-    return contrastive_loss
+def custom_chebyshev_distance(u, v):
+    # Trasformazione degli oggetti in ndarray per poter usare la funzione di scipy.spatial.distance    
+    u = u.cpu().detach().numpy().flatten()
+    v = v.cpu().detach().numpy().flatten()
+    
+    return chebyshev(u, v)
 
 
+def custom_braycurtis_distance(u, v):
+    # Trasformazione degli oggetti in ndarray per poter usare la funzione di scipy.spatial.distance    
+    u = u.cpu().detach().numpy().flatten()
+    v = v.cpu().detach().numpy().flatten()
+    
+    return braycurtis(u, v)
